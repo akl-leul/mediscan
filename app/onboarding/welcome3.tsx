50ada36f05b0c97413beedb9dd06a36c3cd1ab16
Leul Ayfokru <abateisking@gmail.com>
2025-06-23 00:47:48 -0700

    Updated welcome3.tsx

--- app/onboarding/welcome3.tsx
+++ app/onboarding/welcome3.tsx
@@ -61,7 +61,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   imageContainer: {
-    flex: 0.6,
+    flex: 0,
     marginTop: 60,
     borderRadius: 0,
     overflow: 'hidden',
