9880a556f2d0983fcbf7b36438f1e7a886da42ea
Leul Ayfokru <abateisking@gmail.com>
2025-06-23 01:45:27 -0700

    Updated ThemeContext.tsx

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -21,7 +21,7 @@ interface ThemeContextType {
 
 const lightColors = {
   primary: '#3B82F6',
-  background: '#d9d9d9',
+  background: '#cccccc',
   surface: '#F8FAFC',
   text: '#1F2937',
   textSecondary: '#6B7280',
